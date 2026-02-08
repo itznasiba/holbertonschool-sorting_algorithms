b77afe87ca77a611d3728ea890f2effa3d767600
itznasiba <zeynallinesibe2006@gmail.com>
2026-02-08 15:25:17 +0000

    buuble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -4,4 +4,20 @@
 
 bubble_sort(array, n)
 {
+	int i;
+	int j;
+	int temp;
 
+	for (i = 0; i < n - 1; i++)
+	{
+		for (j = 0; j < n - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+			}
+		}
+	}
+}
