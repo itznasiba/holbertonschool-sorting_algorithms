4805f56e2f97a81f3a89beea540e44cebd51e234
itznasiba <zeynallinesibe2006@gmail.com>
2026-02-08 19:34:54 +0000

    insertion sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -4,4 +4,28 @@
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j, min;
-	int tmp;
+	int temp;
+
+	if (!array || size < 2)
+		retun;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+			{
+				min = j;
+			}
+		}
+
+		if (min != i)
+		{
+			temp = array[i];
+			array[i] = array[min];
+			array[min] = temp;
+			print_array(array, size);
+		}
+	}
+}
